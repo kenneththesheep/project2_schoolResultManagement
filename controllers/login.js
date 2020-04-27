1dca023ef9a03be79e92c1e9f9a79708736448f3
kenneththesheep <kenneththesheep@gmail.com>
2020-04-27 21:26:21 +0800

    changed file name

--- controllers/login.js
+++ controllers/login.js
@@ -8,7 +8,7 @@ module.exports = (db) => {
 
   let loginControllerCallback = (request, response) => {
 
-    response.render('home/index');
+    response.render('home/login');
 
   };
   let processLogInControllerCallback = (request, response) => {
@@ -16,13 +16,17 @@ module.exports = (db) => {
     let userLoginDetail= request.body;
       db.login.login(userLoginDetail,(error, returningResult) => {
         if ( returningResult.login){
-
+                var sha256 = require('js-sha256');
+                const secretMessage = 'I Love H20'
                 var userId = request.cookies['userId']
                 var userName = request.cookies['userName'];
+                var loginSession = request.cookies['loginSession'];
                 if(userId === undefined){
                     userId = returningResult.teacherId;
                     userName = returningResult.teacherName;
+                    loginSession = sha256 (userId + 'logged_in' + secretMessage );
                 }
+                response.cookie('loginSession', loginSession)
                 response.cookie('userId', userId);
                 response.cookie('userName', userName);
                 response.send(returningResult);
