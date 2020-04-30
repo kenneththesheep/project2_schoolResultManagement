900effab440db87b97234ebee487ec38e6de2e86
kenneththesheep <kenneththesheep@gmail.com>
2020-05-01 02:33:28 +0800

    Nav bar on every landing page

--- views/formclass/formClassLanding.jsx
+++ views/formclass/formClassLanding.jsx
@@ -1,5 +1,5 @@
 var React = require("react");
-
+var DefaultLayout = require('../layout/navigation')
 class home extends React.Component {
   render() {
 
@@ -16,6 +16,9 @@ class home extends React.Component {
                     <link rel={"stylesheet"} href={"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"} integrity={"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"} crossorigin={"anonymous"}></link>
                         <link rel={"stylesheet"} href={"/style/style.css"}></link>
         <body>
+                      <DefaultLayout title={this.props.title}>
+        <div>{this.props.name}</div>
+      </DefaultLayout>
           <div class={"container mt-3 "}>
             <div class={"row align-bottom mb-5"}>
             <div class={"col-12 mt-5 text-center"}>
