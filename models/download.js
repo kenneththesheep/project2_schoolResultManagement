69a00092d2780f5c34eefc4d6263c2e5a37c938d
kenneththesheep <kenneththesheep@gmail.com>
2020-05-01 12:00:25 +0800

    Change file name

--- models/download.js
+++ models/download.js
@@ -37,7 +37,7 @@ const downloadsFolder = require('downloads-folder');
       const csv = json2csvParser.parse(jsonData);
 
       /*fs.writeFile(downloadDirectory+"/bezkoder_postgresql_fs.csv", csv, function(error)*/
-      fs.writeFile("./bezkoder_postgresql_fs.csv", csv, function(error){
+      fs.writeFile("./student_details.csv", csv, function(error){
         if (error) throw error;
         console.log("student_details.csv successfully!");
         console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
@@ -45,7 +45,7 @@ const downloadsFolder = require('downloads-folder');
       });
             fs.writeFile(downloadDirectory+"/student_details.csv", csv, function(error){
         if (error) throw error;
-        console.log("Write to bezkoder_postgresql_fs.csv successfully!");
+        console.log("Write to student_details.csv successfully!");
         console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         console.log(downloadsFolder());
       });
