971aad8d923ddf7b395d53d2f013cee3d3a92c19
kenneththesheep <kenneththesheep@gmail.com>
2020-05-03 17:17:38 +0800

    styled view conduct page

--- views/conduct/viewConduct.jsx
+++ views/conduct/viewConduct.jsx
@@ -38,10 +38,26 @@ class home extends React.Component {
                       <DefaultLayout title={this.props.title}>
         <div>{this.props.name}</div>
       </DefaultLayout>
-        <div class={"container mt-3 "}>
-        <div class={"row align-bottom mb-5"}>
-            <div class={"col-12 mt-5 text-center"}>
-            <h1  class={"mt-3"}>Class {this.props.class.classname} Conduct Grading and Remarks</h1>
+        <div class={"container mt-3"}>
+        <div class={"row align-bottom"}>
+            <div class={"col-12 LandingTitleBox mb-3 text-center"}>
+            <h1  class={"mt-3 LandingTitleText mb-4 mt-4"} >Class {this.props.class.classname} Conduct Grading and Remarks</h1>
+            </div>
+            </div>
+            <div class = {"row LandingHeaderBox mb-1 pl-2"}>
+
+            <div class = {"col-2 text-center "}>
+                <p class={"mt-2"}>Name</p>
+            </div>
+           <div class = {"col-2 "}>
+                <p class={"mt-2"}>Conduct Grade</p>
+            </div>
+            <div class = {"col-6 text-center"}>
+                <p class={"mt-2"}>Remark</p>
+            </div>
+
+            <div class = {"col-2  "}>
+                  <p class={"mt-2"}>Action</p>
             </div>
             </div>
         </div>
@@ -54,7 +70,7 @@ class home extends React.Component {
 
 
           </div>
-        <div class={"container mt-3 "}>
+        <div class={"container mt-3 longBox"}>
         <div class={"row"}>
             <div class = {"col-3 border mx-auto text-center"}>
                 <button id={"downloadConduct"}>Download</button>
